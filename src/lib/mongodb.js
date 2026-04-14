87870144657b48091a47dd9e8d8c91aa05603b67
Fernando Espinoza <ruferesfer@gmail.com>
2026-04-13 19:37:06 -0600

    Agregué conexión a MongoDB Atlas

---
+++ src/lib/mongodb.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+
+let client;
+let clientPromise;
+
+if (!uri) throw new Error("MONGODB_URI no está definida en .env.local");
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export async function connectDB() {
+  const client = await clientPromise;
+  return client.db("clinicaDentalRadient");
+}
