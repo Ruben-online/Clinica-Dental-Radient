fc30cddf872252a29d22618de3df34256bb9086b
josuebarr10s <jebarrioses@correo.url.edu.gt>
2026-04-27 10:06:58 -0600

    Cambio en viñeta de pagina

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "DentalCare - Tu sonrisa, nuestra pasión",
+  title: "Radient - Tu sonrisa, nuestra pasión",
   description: "Cuidado dental de excelencia con tecnología de vanguardia y un equipo comprometido con tu bienestar.",
 };
 
